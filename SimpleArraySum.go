3297f6f37bf5d84b35eca7cf50f1cbf3491b7b77
satria girinanda <satriagn93@gmail.com>
2019-12-10 11:05:08 +0700

    perbaikan simple array sum

--- SimpleArraySum.go
+++ SimpleArraySum.go
@@ -1,12 +1,13 @@
 package main
+
 import "fmt"
 
-func main(){
-    var n, temp,sum int
-    fmt.Scan(&n)
-    for i:=0;i<n;i++{
-        fmt.Scan(&temp)
-        sum += temp
-    }
-    fmt.Println(sum)
+func main() {
+	var n, temp, sum int
+	fmt.Scan(&n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&temp)
+		sum += temp
+	}
+	fmt.Println(sum)
 }
